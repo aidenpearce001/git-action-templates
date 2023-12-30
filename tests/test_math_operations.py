2e44167612030e9482e08466b40307b3dca81079
Lychee <vietanhnguyen011101@gmail.com>
2023-12-30 15:48:33 +0700

    fix

--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -1,6 +1,6 @@
 # file: test_math_operations.py
 # from app.math_operations import add, divide, multiply, subtract
-from ..app.math_operations import add, divide, multiply, subtract
+from app.math_operations import add, divide, multiply, subtract
 
 def test_add():
     assert add(2, 3) == 5
