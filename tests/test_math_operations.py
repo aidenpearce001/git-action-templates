ea2856363fee5dbd9fad2c891cb1ee1d09fb61eb
aidenpearce001 <aidenpearcewd01@gmail.com>
2024-01-01 11:24:05 +0000

    Unit Test

--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -6,7 +6,7 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
-    assert add(1,100) == 102
+    assert add(1,100) == 101
 
 def test_divide():
     assert divide(2, 2) == 2
